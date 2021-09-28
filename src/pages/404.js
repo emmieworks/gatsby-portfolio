635f764b4e913e99b01931e90ec299b2269437ef
emmieworks <filledforest@gmail.com>
2021-09-28 11:21:56 +0900

    update: 404ページ文章更新

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,18 @@
 import * as React from "react"
-
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import * as button from "../styles/_button.module.scss"
 
 const NotFoundPage = () => (
   <Layout>
     <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1 style={{textAlign:`center`, margin:`2rem auto`}}>404: Not Found</h1>
+    <p style={{textAlign:`center`, margin:`2rem auto`}}>申し訳ありません。ご指定のURLは存在しません。</p>
+    <div style={{textAlign:`center`, margin:`2rem auto`}}>
+      <Link to ="/" className={button.btn}>トップへ戻る</Link>
+    </div>
+
   </Layout>
 )
 
